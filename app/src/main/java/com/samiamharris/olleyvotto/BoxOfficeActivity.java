2ea9ff0d573f86932738ee0156ae5d77a67b21bb
sam-harris-myxer <SamHarris512@gmail.com>
2014-07-01 09:49:25 -0500

    SingleFrag branch commit

--- app/src/main/java/com/samiamharris/olleyvotto/BoxOfficeActivity.java
+++ app/src/main/java/com/samiamharris/olleyvotto/BoxOfficeActivity.java
@@ -2,6 +2,11 @@ package com.samiamharris.olleyvotto;
 
 import android.support.v4.app.Fragment;
 
+/**
+ * this branch is a simple single frag setup that can be used to reference
+ * the simple architecture
+ */
+
 public class BoxOfficeActivity extends SingleFragmentActivity {
 
     @Override
